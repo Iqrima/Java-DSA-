c82bbdaa3a097b6865e5f6c1fa1011fa41a14c53
Iqrima <iqrimakhrty17@gmail.com>
2026-06-04 13:40:35 +0530

    lower and upper case,numbers and symbols

---
+++ Introduction To Java ,inputs,logic,conditions,loops/Assignments code/Casecheck2.java
@@ -0,0 +1,33 @@
+/*
+// this case check program will be for capital letters, small letters
+// ,numbers also for any symbols @##$%!?><  they will print as symbols //
+
+import java.util.Scanner;
+public class Casecheck2 {
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter input");
+        char ch = sc.next().charAt(0);
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("lowercase");
+        } else if (ch >= 'A' && ch <= 'Z') {
+            System.out.println("uppercase");
+        } else if (ch >= '1' && ch <= '9') {
+            System.out.println("number");
+        } else {
+            System.out.println("symbols");
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+*/
