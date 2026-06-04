781082dac1a76593a8322c15105429ea62a78fd0
Iqrima <iqrimakhrty17@gmail.com>
2026-06-04 19:37:27 +0530

    all files

---
+++ Introduction To Java ,inputs,logic,conditions,loops/Assignments code/Hotcold.java
@@ -0,0 +1,17 @@
+/*
+import java.util.Scanner;
+public class Hotcold {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter temperature");
+        int temp = sc.nextInt();
+        if (temp < 30) {
+            System.out.println("cold");
+        } else {
+            System.out.println("hot");
+        }
+    }
+}
+
+
+*/
---
+++ Introduction To Java ,inputs,logic,conditions,loops/Assignments code/Largenum.java
@@ -0,0 +1,29 @@
+/*
+
+
+
+
+import java.util.Scanner;
+public class Largenum {
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a > b ) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+*/
