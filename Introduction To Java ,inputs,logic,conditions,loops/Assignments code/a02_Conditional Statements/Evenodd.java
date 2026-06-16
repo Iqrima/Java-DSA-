b629c5ee4c90b0f13f00fd82e6fa6adf4e1a0df8
Iqrima <iqrimakhrty17@gmail.com>
2026-06-16 16:06:30 +0530

    UPdate

--- Introduction To Java ,inputs,logic,conditions,loops/Assignments code/a02_Conditional Statements/Evenodd.java
+++ Introduction To Java ,inputs,logic,conditions,loops/Assignments code/a02_Conditional Statements/Evenodd.java
@@ -3,7 +3,7 @@
 
 
 import java.util.Scanner;
-public class Main {
+public class Evenodd {
     public static void main (String[] args){
         Scanner sc = new Scanner (System.in);
         int num = sc.nextInt();
@@ -14,8 +14,6 @@ public class Main {
         }
 
     }
-
-}
 
 
 
