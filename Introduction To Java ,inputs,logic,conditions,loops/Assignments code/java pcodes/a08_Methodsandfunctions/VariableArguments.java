917667015716ff36bdd7c8778c803dca9b10e7d0
Iqrima <iqrimakhrty17@gmail.com>
2026-07-01 17:13:38 +0530

    added new

---
+++ Introduction To Java ,inputs,logic,conditions,loops/Assignments code/java pcodes/a08_Methodsandfunctions/VariableArguments.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class VariableArguments {
+   public static void main(String[] args) {
+    fun(2,3,5,6,17,07);  // nums inside are arguements ,,fun()name of method  called from main
+    }
+
+    static void fun(int...v){          // created parameter to for above called ,method here
+        System.out.println(Arrays.toString(v));
+    }
+
+}
+//Varaibles store the values
+//arguments valuepasss into a methods
